f1f392f42d38f59fe35a1f6f115601aca625ac65
张哲恺 <sxtyzhangzk@live.cn>
2017-04-22 00:00:10 +0800

    Maybe use -O1 ?

--- build.bash
+++ build.bash
@@ -4,6 +4,6 @@ mkdir -p build/generated
 cd rules
 java -jar ../lib/antlr-4.7-complete.jar -Dlanguage=Cpp MxLexer.g4 MxParser.g4 -o ../build/generated/ -no-listener -visitor
 cd ../build
-tar -zxvf ../lib/antlr4-runtime.tar.gz
+tar -zxf ../lib/antlr4-runtime.tar.gz
 cp ../lib/Makefile .
 make -j2
