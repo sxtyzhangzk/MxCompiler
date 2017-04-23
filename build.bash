a0f0326654a44b0f6ba9fc2dd20588f2e0e21c4a
张哲恺 <sxtyzhangzk@live.cn>
2017-04-23 22:47:21 +0800

    Test Time

--- build.bash
+++ build.bash
@@ -2,9 +2,13 @@ set -e
 rm -rf build
 mkdir -p build/generated
 cd rules
-java -jar ../lib/antlr-4.7-complete.jar -Dlanguage=Cpp MxLexer.g4 MxParser.g4 -o ../build/generated/ -no-listener -visitor
+echo generating parser...
+time java -jar ../lib/antlr-4.7-complete.jar -Dlanguage=Cpp MxLexer.g4 MxParser.g4 -o ../build/generated/ -no-listener -visitor
 cd ../build
-tar -zxf ../lib/antlr4-runtime.tar.gz
-tar -zxf ../lib/boost_mini_1_64_0.tar.gz
+echo extracting antlr4...
+time tar -zxf ../lib/antlr4-runtime.tar.gz
+echo extracting boost...
+time tar -zxf ../lib/boost_mini_1_64_0.tar.gz
 cp ../lib/Makefile .
-make
+echo compiling...
+time make
