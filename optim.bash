37ea26a62c40744c33df173a471fa4949ddf64b1
张哲恺 <sxtyzhangzk@live.cn>
2017-05-28 18:13:28 +0800

    reopen the optim flag

--- optim.bash
+++ optim.bash
@@ -1,4 +1,4 @@
 cd build
 cat > program.mx
-./mxcompiler program.mx -o program.asm --fdisable-access-protect --optim-reg-alloc
+./mxcompiler program.mx -o program.asm --fdisable-access-protect --optim-reg-alloc --optim-inline --optim-loop-invariant --optim-dead-code
 cat program.asm
